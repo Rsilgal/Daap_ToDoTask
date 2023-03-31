2d035253effbd38eba899b778e52f1433625aacd
Ramon Silvente Galera <rsilgal@gmail.com>
2023-03-31 10:01:45 +0200

    [ADD] Structure of tests

---
+++ test/ToDo.ts
@@ -0,0 +1,75 @@
+import hre from 'hardhat'
+
+describe("ToDo", function () {
+    before(function() {
+        // Run once before the first test
+    });
+
+    describe("Create a task", function() {
+        it("with the right params", function () {
+
+        })
+
+        it("without description",  function() {
+
+        })
+
+        it("without title", function() {
+            
+        })
+    });
+
+    describe("Edit a task and sender is the owner", function() {
+        it("with the right params", function() {
+
+        })
+
+        it("task does not exist", function() {
+
+        })
+
+        it("without id param", function() {
+
+        })
+
+        it("without deleted param", function() {
+
+        })
+
+        it("without finished param", function() {
+
+        })
+
+        it("without description param", function() {
+
+        })
+
+        it("without title param", function() {
+
+        })
+
+        it("without change task", function() {
+
+        })
+    });
+
+    describe("Edit a task and sender is not the owner", function() {
+        it("", function(){
+
+        });
+    });
+
+    describe("Get task data", function() {
+        it("by id and sender is the owner", function() {
+
+        });
+
+        it("by id and sender is not the owner", function() {
+
+        });
+
+        it("id task does not exist", function() {
+
+        });
+    })
+})
